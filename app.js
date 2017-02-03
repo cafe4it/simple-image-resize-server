c3650d4ca5d1150cc55289936e04f3bca75899cf
Cafe4it <cafe4it@gmail.com>
2017-02-03 16:14:39 +0700

    init

---
+++ app.js
@@ -0,0 +1,74 @@
+var express = require('express'),
+	expressValidator = require('express-validator')
+var util = require('util'),
+	bodyParser = require('body-parser')
+
+var mime = require('mime-types')
+var http = require('http');
+var https = require('https');
+
+var sharp = require('sharp')
+
+var app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(expressValidator())
+
+app.get('/', function (req, res) {
+	res.json({
+		isReady: true
+	})
+})
+
+app.get('/resize', function (req, res) {
+	req.checkQuery('width').notEmpty().isInt()
+	req.checkQuery('height').optional(true).isInt()
+	req.checkQuery('image_url').notEmpty()
+
+	req.getValidationResult().then(function (result) {
+		if (!result.isEmpty()) {
+			res.status(400).send('There have been validation errors: ' + util.inspect(result.array()));
+			return;
+		}
+		var width = req.query['width']
+		var height = req.query['height'] ? req.query['height'] : width
+		var imageUri = req.query['image_url']
+		width = parseInt(width)
+		height = parseInt(height)
+
+		var httpLib = http;
+		if (/^https/.test(imageUri)) {
+			httpLib = https;
+		}
+		const filename = imageUri.split('/').pop()
+		const ext = filename.split('.').pop()
+		const resMime = mime.lookup(ext)
+		var resizeTransform = sharp().resize(width, height).background({r: 0, g: 0, b: 0, alpha: 0})
+			.embed()
+		res.writeHead(200, {
+			'Content-Type': resMime,
+			'Content-disposition': 'attachment;filename=' + filename
+		});
+		httpLib.get(imageUri, function (downloadStream) {
+			downloadStream.pipe(resizeTransform).pipe(res)
+			downloadStream.on('end', function () {
+				console.log('downloadStream', 'END');
+			});
+
+			downloadStream.on('error', function (err) {
+				console.log('download error', err);
+			});
+
+			resizeTransform.on('error', function (err) {
+				console.log('resizeTransform', err);
+			});
+		});
+	})
+})
+
+var port = process.env.PORT || 3000
+
+app.listen(port, function () {
+	console.log('App listening on port:' + port)
+})
